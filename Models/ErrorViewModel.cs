a9a9a84fb115d4525f1a3d6547ff073c72af99b5
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2021-02-09 11:15:35 +0100

    initial checkin

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RB2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
